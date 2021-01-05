e078186e31de8d4374513c22446e00bcd8e2b672
Roxana C <roxana.c2007@gmail.com>
2021-01-05 23:24:38 +0000

    timer function

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 
 
 function displayCountdown(){
-    let countBox = document.querySelector(".countBox")
+   let countBox = document.querySelector(".countBox")
    let days = document.createElement("div")
    days.className = "days"
    let hours = document.createElement("div")
@@ -20,12 +20,58 @@ function displayCountdown(){
 
    countBox.appendChild(days)
    countBox.append(hours)
-    countBox.append(minutes)
-    countBox.append(seconds)
-    console.log(countBox)
+   countBox.append(minutes)
+   countBox.append(seconds)
+   console.log(countBox)
 
 }
 
+function displayTime(){
+    let d;
+    let h;
+    let m;
+    let s;
 
+    setInterval(function(){
+      // d = 8
+      // h = 23
+      // m = 55
+      // s = 41
+      
+
+  
+
+      d = d < 10 ? "0" + d : d;
+      h = h < 10 ? "0" + h : h;
+      m = m < 10 ? "0" + m : m;
+      s = s < 10 ? "0" + s : s;
+
+      if(s == 00){
+        s = 59;
+        s--;
+      }else{
+        s= 0;
+      }
+      if(m == 00 ){
+        m = 59;
+        m--
+      }
+
+      // if(h == 00){
+      //     h = 23;
+      //     d --;
+      // }
+      
+
+//       console.log(d)
+//       console.log(h)
+// console.log(m)
+// console.log(s)
+    }, 1000)
+    console.log(`${d} : ${h} : ${m} : ${s}`)
+     return `${d} : ${h} : ${m} : ${s}`
+    
+}
 
 displayCountdown()
+displayTime()
